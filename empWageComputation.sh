b5df7eb46d6a934948170a11f223ebbbf69573c3
naveeny061 <naveeny061@gmail.com>
2020-10-18 17:40:07 +0530

    adding attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+attendence=$((RANDOM%2))
+if [ $attendence -eq 0 ]
+then
+echo "employee present"
+else
+echo "employee absent"
+fi
