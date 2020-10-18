1742b43db9c90491a30da97a6b61709a7643f84e
naveeny061 <naveeny061@gmail.com>
2020-10-18 19:02:36 +0530

    adding conditions max days 20 and max hours 100

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,23 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-attendence=$((RANDOM%2))
-if [ $attendence -eq 0 ]
-then
-echo "employee present"
-else
-echo "employee absent"
-fi
 wagePerHour=20
 fullDayHour=8
 partTimeHours=4
-if [ $attendence -eq 0 ]
-then
+totalDays=0
+workingHours=0
+totalWage=0
 read -p "enter 1 for full day and 2 for part time" choise
+while [[ $totalDays -lt 20 && $workingHours -lt 100 ]]
+do
 case $choise in
 1)dailyEmployeeWage=$((wagePerHour*fullDayHour))
-monthSalary=$((dailyEmployeeWage*20));;
+totalWage=$((totalWage+dailyEmployeeWage))
+workingHours=$((workingHours+8));;
 2)dailyEmployeeWagePartTime=$((wagePerHour*partTimeHours))
-monthSalary=$((dailyEmployeeWagePartTime*20));;
+totalWage=$((totalWage+dailyEmployeeWagePartTime))
+workingHours=$((workingHours+4));;
 esac
-else
-monthSalary=0
-fi
-echo "monthSalary=" $monthSalary
+((totalDays++))
+done
+echo "totalWage=" $totalWage
+
