3879b80914e81be0d5c179335673885d2ce052ce
naveeny061 <naveeny061@gmail.com>
2020-10-18 19:41:14 +0530

    adding daily day wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,26 +3,30 @@ echo "Welcome to Employee Wage Computation Program"
 wagePerHour=20
 fullDayHour=8
 partTimeHours=4
-totalDays=0
+totalDays=1
 workingHours=0
 totalWage=0
 workHours()
 {
 read -p"enter 1 for full day and 2 for part time" choise
-while [[ $totalDays -lt 20 && $workingHours -lt 100 ]]
+while [[ $totalDays -le 20 && $workingHours -lt 100 ]]
 do
 case $choise in
 1)dailyEmployeeWage=$((wagePerHour*fullDayHour))
 totalWage=$((totalWage+dailyEmployeeWage))
-workingHours=$((workingHours+8));;
+workingHours=$((workingHours+8))
+echo "day no.=" $totalDays
+echo "daily wage=" $dailyEmployeeWage
+echo "wage till now=" $totalWage;;
 2)dailyEmployeeWage=$((wagePerHour*partTimeHours))
 totalWage=$((totalWage+dailyEmployeeWage))
-workingHours=$((workingHours+4));;
+workingHours=$((workingHours+4))
+echo "day no.=" $totalDays
+echo "daily wage=" $dailyEmployeeWage
+echo "wage till now=" $totalWage;;
 esac
 ((totalDays++))
 done
-echo "totalWage=" $totalWage
-echo "total hours=" $workingHours
-echo "daily wage=" $dailyEmployeeWage
+
 }
 workHours
