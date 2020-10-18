5172decf93fdf702cf1fd2a43a76ff2fab548886
naveeny061 <naveeny061@gmail.com>
2020-10-18 18:19:25 +0530

    calculating using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,8 +9,16 @@ echo "employee absent"
 fi
 wagePerHour=20
 fullDayHour=8
-dailyEmployeeWage=$((wagePerHour*fullDayHour))
-echo "daily employee wage =" $dailyEmployeeWage
 partTimeHours=4
-dailyEmployeeWagePartTime=$((wagePerHour*partTimeHours))
-echo "part time daily employee wage =" $dailyEmployeeWagePartTime
+if [ $attendence -eq 0 ]
+then
+read -p "enter 1 for full day and 2 for part time" choise
+case $choise in
+1)dailyEmployeeWage=$((wagePerHour*fullDayHour))
+echo "daily employee wage =" $dailyEmployeeWage;;
+2)dailyEmployeeWagePartTime=$((wagePerHour*partTimeHours))
+echo "part time daily employee wage =" $dailyEmployeeWagePartTime;;
+esac
+else 
+echo "no wage "
+fi
